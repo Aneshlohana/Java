5c481af91be83770e09108aa179d9ce429bfc67b
Anesh  kumar <91781521+Aneshlohana@users.noreply.github.com>
2022-04-15 20:15:38 +0500

    Add files via upload

---
+++ Product_of_two_number.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Product_of_two_number {
+    public static void main(String[] args) {
+        Scanner Sc = new Scanner(System.in);
+        System.out.println("Enter number a : ");
+        int a =Sc.nextInt();
+        System.out.println("Enter number b : ");
+        int b =Sc.nextInt();
+        System.out.printf("The Product of two number is : %d", a*b );
+    }
+}
